8b68b59114df88bdd8ae5089265e5c3d01a768f9
watts <devnull@localhost>
2010-07-07 20:54:36 +0000

    HD monitor now can handle removable drives correctly. #4368

--- pr2_computer_monitor/scripts/hd_monitor.py
+++ pr2_computer_monitor/scripts/hd_monitor.py
@@ -61,6 +61,7 @@ stat_dict = { 0: 'OK', 1: 'Warning', 2: 'Error' }
 temp_dict = { 0: 'OK', 1: 'Hot', 2: 'Critical Hot' }
 usage_dict = { 0: 'OK', 1: 'Low Disk Space', 2: 'Very Low Disk Space' }
 
+REMOVABLE = ['/dev/sg1', '/dev/sdb'] # Store removable drives so we can ignore if removed
 
 ## Connects to hddtemp daemon to get temp, HD make.
 def get_hddtemp_data(hostname = 'localhost', port = 7634):
@@ -167,8 +168,6 @@ class hd_monitor():
 
         self.check_temps()
 
-
-        
     ## Must have the lock to cancel everything
     def cancel_timers(self):
         if self._temp_timer:
@@ -195,9 +194,12 @@ class hd_monitor():
         for index in range(0, len(drives)):
             temp = temps[index]
             
-            if not unicode(temp).isnumeric():
+            if not unicode(temp).isnumeric() and drives[index] not in REMOVABLE:
                 temp_level = DiagnosticStatus.ERROR
                 temp_ok = False
+            elif not unicode(temp).isnumeric() and drives[index] in REMOVABLE:
+                temp_level = DiagnosticStatus.OK
+                temp = "Removed"
             else:
                 temp_level = DiagnosticStatus.OK
                 if float(temp) > hd_temp_warn:
